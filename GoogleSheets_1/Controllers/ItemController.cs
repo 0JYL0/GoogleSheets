dc139613c89d50689f4872cc35fb90b7269bf10c
0JYL0 <jayalmalusare.912.jm@gmail.com>
2022-10-29 15:55:29 +0530

    Added All Files

---
+++ GoogleSheets_1/Controllers/ItemController.cs
@@ -0,0 +1,117 @@
+﻿using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Sheets.v4;
+using Microsoft.AspNetCore.Mvc;
+using static Google.Apis.Sheets.v4.SpreadsheetsResource.ValuesResource;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace GoogleSheets_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        const string SPREADSHEET_ID = "1Tg_GUhaJBzAY8s-wjwV9zidxjj0DCXrcmgLzaxfHt2o";
+        const string SHEET_NAME = "Item";
+
+        SpreadsheetsResource.ValuesResource _googleSheetValues;
+
+        public ItemsController(GoogleSheetsHelper googleSheetsHelper)
+        {
+            _googleSheetValues = googleSheetsHelper.Service.Spreadsheets.Values;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var range = $"A:D";
+
+            var request = _googleSheetValues.Get(SPREADSHEET_ID, range);
+            var response = request.Execute();
+            var values = response.Values;
+
+            return Ok(ItemsMapper.MapFromRangeData(values));
+        }
+
+        [HttpGet("{rowId}")]
+        public IActionResult Get(int rowId)
+        {
+            var range = $"A{rowId}:D{rowId}";
+            var request = _googleSheetValues.Get(SPREADSHEET_ID, range);
+            var response = request.Execute();
+            var values = response.Values;
+
+            return Ok(ItemsMapper.MapFromRangeData(values).FirstOrDefault());
+        }
+
+        [HttpPost]
+        public IActionResult Post(List<Item> item)
+        {
+            try
+            {
+
+                var range = $"A:D";
+                var valueRange = new ValueRange
+                {
+                    Values = ItemsMapper.MapToRangeData(item)
+                };
+
+                var appendRequest = _googleSheetValues.Append(valueRange, SPREADSHEET_ID, range);
+                appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.RAW;
+                appendRequest.InsertDataOption = AppendRequest.InsertDataOptionEnum.INSERTROWS;
+                appendRequest.Execute();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return CreatedAtAction(nameof(Get), item);
+        }
+
+        [HttpPut("{rowId}")]
+        public IActionResult Put(int rowId, List<Item> item)
+        {
+            var range = $"A{rowId}:D{rowId}";
+            var valueRange = new ValueRange
+            {
+                Values = ItemsMapper.MapToRangeData(item)
+            };
+
+            var updateRequest = _googleSheetValues.Update(valueRange, SPREADSHEET_ID, range);
+            updateRequest.ValueInputOption = UpdateRequest.ValueInputOptionEnum.USERENTERED;
+            updateRequest.Execute();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{rowId}")]
+        public IActionResult Delete(int rowId)
+        {
+            var range = $"A{rowId}:D{rowId}";
+            var requestBody = new ClearValuesRequest();
+
+            var deleteRequest = _googleSheetValues.Clear(requestBody, SPREADSHEET_ID, range);
+            deleteRequest.Execute();
+
+            return NoContent();
+        }
+
+        [HttpDelete("")]
+        public IActionResult DeleteAll()
+        {
+            var request = _googleSheetValues.Get(SPREADSHEET_ID, "A:D");
+            var response = request.Execute();
+            var val = response.Values.Count;
+
+            var range = $"A{2}:D{val}";
+            var requestBody = new ClearValuesRequest();
+
+            var deleteRequest = _googleSheetValues.Clear(requestBody, SPREADSHEET_ID, range);
+            deleteRequest.Execute();
+
+            return NoContent();
+        }
+    }
+}
---
+++ GoogleSheets_1/GoogleSheetsHelper.cs
@@ -0,0 +1,91 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GoogleSheets_1
+{
+    public class GoogleSheetsHelper
+    {
+        public SheetsService Service { get; set; }
+        const string APPLICATION_NAME = "GroceryStore";
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
+        public GoogleSheetsHelper()
+        {
+            InitializeService();
+        }
+
+        private void InitializeService()
+        {
+            var credential = GetCredentialsFromFile();
+            Service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = APPLICATION_NAME
+            });
+        }
+
+        private GoogleCredential GetCredentialsFromFile()
+        {
+            GoogleCredential credential;
+            using (var stream = new FileStream("secertkey-2a110a4e6a8e.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            return credential;
+        }
+    }
+
+    //Model for data gathering
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+    }
+
+    //--------
+    public static class ItemsMapper
+    {
+        public static List<Item> MapFromRangeData(IList<IList<object>> values)
+        {
+            var items = new List<Item>();
+
+            foreach (var value in values)
+            {
+                Item item = new Item()
+                {
+                    Id = value[0].ToString(),
+                    Name = value[1].ToString(),
+                    Category = value[2].ToString(),
+                    Price = value[3].ToString()
+                };
+
+                items.Add(item);
+            }
+
+            return items;
+        }
+
+        public static IList<IList<Item>> MapToRangeData(List<Item> item)
+        {
+
+            var objectList = new List<Item>();
+
+            foreach(var Itemo in item)
+            {
+                objectList.Add(Itemo);
+            }
+
+            List<IList<Item>>  rangeData = new List<IList<Item>> { objectList };
+            Console.WriteLine(rangeData);
+          
+            return rangeData;
+        }
+    }
+}
